bf31958ccdae66163e58bc871761a503feb06279
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-01-30 12:06:49 +0300

    added pizzas array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
+import Header from './components/Header';
+import Pizza from './components/Pizza';
 
 const App = () => {
-    return (
-        <div>
-            App
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
+      <Pizza />
+    </div>
+  );
+};
 
-export default App
+export default App;
